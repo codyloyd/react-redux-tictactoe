c1184472d932abdaa5eda1eea82b02af68986f53
Cody Loyd <codyloyd@gmail.com>
2017-02-03 07:56:41 -0600

    lint

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { createStore, combineReducers } from 'redux'
-import './App.css';
+import './App.css'
 
-//redux reducers:
-const gameboard = (state=[null,null,null,null,null,null,null,null,null],action) => {
-  switch(action.type){
+// redux reducers:
+const gameboard = (state = [null, null, null, null, null, null, null, null, null], action) => {
+  switch (action.type) {
     case 'PLACE_MARKER':
-      return state.map((x,i) => 
-        i === action.positon ? action.marker : x);
+      return state.map((x, i) =>
+        i === action.positon ? action.marker : x)
     case 'RESTART_GAME':
-      return state.map(x => null)  
+      return state.map(x => null)
     default:
-    return state;  
+      return state
   }
 }
 
-const currentPlayer = (state='X',action) => {
-  switch(action.type){
+const currentPlayer = (state = 'X', action) => {
+  switch (action.type) {
     case 'SWITCH_PLAYER':
-      return state === 'X' ? 'O' : 'X';
+      return state === 'X' ? 'O' : 'X'
     default:
-      return state;
+      return state
   }
 }
 
-const game = combineReducers({gameboard,currentPlayer})
-const store = createStore(game);
+const game = combineReducers({gameboard, currentPlayer})
+const store = createStore(game)
 
-//React Components
-const Square = ({marker,onclick}) => 
-  <div 
+// React Components
+const Square = ({marker, onclick}) =>
+  <div
     className="square"
     onClick={() => onclick()}
     >{marker}</div>
@@ -44,42 +44,40 @@ const gameStatus = board => {
                          [2, 5, 8],
                          [0, 4, 8],
                          [2, 4, 6]]
-  if (board.every(x => x)) return {win:false}                      
-  return winConditions.reduce((win,c) => {
-    if (!board[c[0]]) return win;
-    if (board[c[0]] === board[c[1]] && 
-        board[c[0]] === board[c[2]]) 
-      {
-        return {win: true, winner: board[c[0]]}
-      };
-    return win;
-  }, {win:false, winner:null})
+  if (board.every(x => x)) return {win: false}
+  return winConditions.reduce((win, c) => {
+    if (!board[c[0]]) return win
+    if (board[c[0]] === board[c[1]] &&
+        board[c[0]] === board[c[2]]) {
+      return {win: true, winner: board[c[0]]}
+    };
+    return win
+  }, {win: false, winner: null})
 }
 
 const Board = ({board}) => {
-  return(
+  return (
     <div className="board">
-      {board.map((x,i) => 
-        <Square 
+      {board.map((x, i) =>
+        <Square
           marker={x}
           key={i}
           onclick={() => {
-              if (!board[i] && !gameStatus(board).win) {
-                store.dispatch({
-                  type: 'PLACE_MARKER',
-                  positon: i,
-                  marker: store.getState().currentPlayer
-                })
-                store.dispatch({type: 'SWITCH_PLAYER'})
-              }
+            if (!board[i] && !gameStatus(board).win) {
+              store.dispatch({
+                type: 'PLACE_MARKER',
+                positon: i,
+                marker: store.getState().currentPlayer
+              })
+              store.dispatch({type: 'SWITCH_PLAYER'})
             }
+          }
           }
         />
       )}
     </div>
   )
 }
-
 
 const Gameover = ({board}) => {
   if (gameStatus(board).win) {
@@ -90,47 +88,48 @@ const Gameover = ({board}) => {
         <RestartButton />
       </div>
     )
-  } else if (gameStatus(board).tie){
+  } else if (gameStatus(board).tie) {
     return (
       <div>
         <h1>GAMEOVER DUDE</h1>
         <h2>it was a tie</h2>
         <RestartButton />
       </div>
-    )  }
+    )
+  }
   return <div></div>
 }
 
 const RestartButton = props => {
   return (
-    <button 
+    <button
       onClick={() =>
         store.dispatch({
-          type:'RESTART_GAME'
+          type: 'RESTART_GAME'
         })
       }
-    >RESTART GAME</button>    
+    >RESTART GAME</button>
   )
 }
 
 const Title = props => <h1>tictactoe</h1>
 
-const App  = props => {
+const App = props => {
   return (
     <div>
       <Title />
       <Board board={store.getState().gameboard} />
       <Gameover board={store.getState().gameboard}/>
     </div>
-  );
+  )
 }
 
 const render = () => {
   ReactDOM.render(
     <App />,
     document.getElementById('root')
-  );
+  )
 }
 
 store.subscribe(render)
-export default App;
+export default App
